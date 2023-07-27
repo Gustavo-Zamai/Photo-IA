bd462be7b58bd4d01598a5938ccab9000bf2ba30
Gustavo-Zamai <gustavosimaozamai@gmail.com>
2023-07-27 01:29:57 -0300

    Add project to github

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+const slider = document.getElementById("slider");
+const before = document.querySelector (".before-image");
+const dragger = document.getElementById('dragger');
+
+
+slider.addEventListener("input", (e) => {
+    before.style.width = e.target.value + '%';
+
+    dragger.style.left = `calc(${e.target.value}%)`;
+});
